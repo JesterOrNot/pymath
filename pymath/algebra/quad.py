af9c3e8a4f313972d522c4a7818b4dff812a819a
Sean Hellum <seanhellum45@gmail.com>
2019-10-01 01:36:28 +0000

    removed the need of numpy

--- pymath/algebra/quad.py
+++ pymath/algebra/quad.py
@@ -1,7 +1,7 @@
-import numpy as np
+import math
 ''' This module returns a tuple with 2 values x1 and x2'''
 def quad(a,b,c):
     '''This does the computing'''
-    x_int_1 = (-b + np.sqrt(b**2-4*a*c))/2*a
-    x_int_2 = (-b - np.sqrt(b**2-4*a*c))/2*a
+    x_int_1 = (-b + math.sqrt(b**2-4*a*c))/2*a
+    x_int_2 = (-b - math.sqrt(b**2-4*a*c))/2*a
     return x_int_1, x_int_2
