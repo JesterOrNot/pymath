b361beae6198beab4037b519d4e9c84864c65eec
Sean Hellum <seanhellum45@gmail.com>
2019-10-06 12:42:43 +0000

    add tangental acceleration

--- pymath/physics/__init__.py
+++ pymath/physics/__init__.py
@@ -3,3 +3,4 @@ from .displacement import displacement
 from .speed import speed
 from .acceleration import acceleration
 from .tangental_speed import tangental_speed
+from .tangental_acceleration import tangental_acceleration
---
+++ pymath/physics/tangental_acceleration.py
@@ -0,0 +1,4 @@
+from math import pi
+def tangental_acceleration(radius,time,cycles):
+    awns = (4 * pi**2 * radius) / (time / cycles)**2
+    return awns
