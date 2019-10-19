fba5472c7c3cfa840c149c325f6918e5dde3d076
Sean Hellum <seanhellum45@gmail.com>
2019-10-19 17:36:23 +0000

    stuff

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ from pymath.algebra import log as log
 
 def test_log():
     """Tests pymath.algebra.log."""
-    assert log(3, 77) == 0.25291471002384025, "Should be 0.25291471002384025"
+    assert log(4, 16) == 2, "Should be 2"
 
 
 if __name__ == "__main__":
