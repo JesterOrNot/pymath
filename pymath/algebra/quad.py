29e4b0987c387798407f2163088070ec42d71f2c
Sean Hellum <seanhellum45@gmail.com>
2019-11-07 16:07:07 +0000

    add handling for complex numbers

--- pymath/algebra/quad.py
+++ pymath/algebra/quad.py
@@ -1,15 +1,16 @@
 """Returns a tuple with 2 values x1 and x2."""
 import math
+import cmath
 
 
 def quad(a, b, c):
     """Compute the equation."""
     try:
         x_int_1 = (-b + math.sqrt(b**2-4*a*c))/2*a
-    except Exception:
-        x_int_1 = None
+    except ValueError:
+        x_int_1 = (-b + cmath.sqrt(b**2-4*a*c))/2*a
     try:
         x_int_2 = (-b - math.sqrt(b**2-4*a*c))/2*a
-    except Exception:
-        x_int_2 = None
+    except ValueError:
+        x_int_2 = (-b + cmath.sqrt(b**2-4*a*c))/2*a
     return x_int_1, x_int_2
