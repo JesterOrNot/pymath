8271e4171813c09b3fddd880940772c4d8aef215
Sean Hellum <seanhellum45@gmail.com>
2019-11-07 15:57:37 +0000

    update

--- pymath/algebra/quad.py
+++ pymath/algebra/quad.py
@@ -1,15 +1,15 @@
+"""Returns a tuple with 2 values x1 and x2."""
 import math
-''' This module returns a tuple with 2 values x1 and x2'''
 
 
 def quad(a, b, c):
-    '''This does the computing'''
+    """Compute the equation."""
     try:
         x_int_1 = (-b + math.sqrt(b**2-4*a*c))/2*a
-    except:
+    except Exception:
         x_int_1 = None
     try:
         x_int_2 = (-b - math.sqrt(b**2-4*a*c))/2*a
-    except:
+    except Exception:
         x_int_2 = None
     return x_int_1, x_int_2
