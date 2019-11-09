f92028ffb1a9712eeea80e28adcb6fd33f16edbe
Sean Hellum <seanhellum45@gmail.com>
2019-11-09 21:02:07 +0000

    initialize code skeleton for windows app

--- app/windows/main.cs
+++ app/windows/main.cs
@@ -2,7 +2,21 @@ using System;
 namespace app {
     class app {
         public static void Main(string[] args) {
-            Console.WriteLine("WIP");
+            Console.Write("Welcome to pymath available calculators include algebra, chemistry, geometry, physics\nWhich do you want?: ")
+            Console.ReadLine();
+        }
+        public static void algebra() {
+            Console.Write("Welcome to the algebra part of pymath available calculators include logorithm calc(log), and quadratic equation calc(quad)!\nWhich do you want?: ");
+            Console.ReadLine();
+        }
+        public static void geometry() {
+            // WIP
+        }
+        public static void physics() {
+            // WIP
+        }
+        public static void chemistry() {
+            // WIP
         }
     }
 }
