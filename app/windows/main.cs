13a6436d422b97f65e2b6115489f4a70473aaae6
Sean Hellum <seanhellum45@gmail.com>
2019-11-09 21:12:00 +0000

    add opening print statements

--- app/windows/main.cs
+++ app/windows/main.cs
@@ -3,17 +3,18 @@ namespace app {
     class app {
         public static void Main(string[] args) {
             Console.Write("Welcome to pymath available calculators include algebra, chemistry, geometry, physics\nWhich do you want?: ")
-            Console.ReadLine();
+            input = Console.ReadLine();
         }
         public static void algebra() {
             Console.Write("Welcome to the algebra part of pymath available calculators include logorithm calc(log), and quadratic equation calc(quad)!\nWhich do you want?: ");
-            Console.ReadLine();
+            input = Console.ReadLine();
         }
         public static void geometry() {
-            // WIP
+            Console.Write("Welcome to the geometry part of pymath available calculators include circumfrence calculator(circ), pythagorean theorem calc(pythag), area calc(area)\nWhich do you want?: ");
+            input = Console.ReadLine();
         }
         public static void physics() {
-            // WIP
+            Console.Write("Welcome to the physics portion of pymath! available calculators include acceleration calc(acc), displacement calculator(disp), speed calc(speed), tangental acceleration calc(tangacc), velocity calc (velocity), tangental speed calc(tanspeed)\nWhich do you want?: ");
         }
         public static void chemistry() {
             // WIP
