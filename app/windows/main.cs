795f89d10e767eede1b8f9fde0cadbf7580f812a
Sean Hellum <seanhellum45@gmail.com>
2019-11-11 00:43:49 +0000

    update

--- app/windows/main.cs
+++ app/windows/main.cs
@@ -1,24 +1,44 @@
 using System;
-namespace app {
-    class app {
-        public static void Main(string[] args) {
-            Console.Write("Welcome to pymath available calculators include algebra, chemistry, geometry, physics\nWhich do you want?: ")
-            input = Console.ReadLine();
+using System.Diagnostics;
+namespace app
+{
+    class app
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Welcome to pymath available calculators include algebra, chemistry, geometry, physics\nWhich do you want?: ");
+            string input = Console.ReadLine();
         }
-        public static void algebra() {
+        public static void algebra()
+        {
             Console.Write("Welcome to the algebra part of pymath available calculators include logorithm calc(log), and quadratic equation calc(quad)!\nWhich do you want?: ");
-            input = Console.ReadLine();
+            string input = Console.ReadLine();
+            if (input == "log")
+            {
+                ExecuteSysCmd("python3","app/lib/logexe.py");
+            }
         }
-        public static void geometry() {
+        public static void geometry()
+        {
             Console.Write("Welcome to the geometry part of pymath available calculators include circumfrence calculator(circ), pythagorean theorem calc(pythag), area calc(area)\nWhich do you want?: ");
-            input = Console.ReadLine();
+            string input = Console.ReadLine();
         }
-        public static void physics() {
+        public static void physics()
+        {
             Console.Write("Welcome to the physics portion of pymath! available calculators include acceleration calc(acc), displacement calculator(disp), speed calc(speed), tangental acceleration calc(tangacc), velocity calc (velocity), tangental speed calc(tanspeed)\nWhich do you want?: ");
-            input = Console.ReadLine();
+            string input = Console.ReadLine();
         }
-        public static void chemistry() {
+        public static void chemistry()
+        {
             // WIP
+        }
+        public static void ExecuteSysCmd(string command, string args = "")
+        {
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = command;
+            proc.StartInfo.Arguments = args;
+            proc.Start();
+            proc.WaitForExit();
         }
     }
 }
